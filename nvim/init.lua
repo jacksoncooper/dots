33534c4e298c0a9762d5cc804670b7e7e33760c9
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2023-07-01 16:38:40 -0700

    Enable clipboard over `ssh` and `tmux`

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,34 @@
-require("plugins")
-require("languages")
+require('plugins')
+require('languages')
 
 vim.opt.termguicolors = true
 vim.opt.relativenumber = true
 vim.opt.number = true
 
-vim.o.background = "light" -- or "light" for light mode
-vim.cmd("colorscheme catppuccin-latte")
+vim.lsp.set_log_level('debug')
+vim.opt.listchars = {
+  eol = '¬',
+  tab = '>-',
+  trail = '~',
+  extends = '→',
+  precedes = '←',
+  space = '·',
+}
+vim.opt.list = true
+
+-- TODO: This will break the clipboard when not in `tmux`
+vim.opt_global.clipboard = {
+  name = 'tmux-clipboard',
+  copy = {
+    ['+'] = { 'tmux', 'save-buffer', '-' },
+    ['*'] = { 'tmux', 'save-buffer', '-' },
+  },
+  paste = {
+    ['+'] = { 'tmux', 'save-buffer', '-' },
+    ['*'] = { 'tmux', 'save-buffer', '-' },
+  },
+  cache_enabled = 1,
+}
+
+vim.o.background = 'light' -- or "light" for light mode
+vim.cmd('colorscheme catppuccin-latte')
